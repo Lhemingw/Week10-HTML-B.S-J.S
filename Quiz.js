6ff26203bc3d705d56cb4fef4d0280fec2ccb571
Lhemingw <lhemingw@gmail.com>
2023-08-16 21:02:37 -0600

    Add new paragraph-and clear

--- Quiz.js
+++ Quiz.js
@@ -1,11 +1,16 @@
-let paragraphld = 0;
-document.getElementById('p-button').addEventListener('click',()=>{
-let parent= document.getElementById('p-button');
+//    When the button is clicked, a new paragraph should be added to the div.
+//The content of the paragraph should be the text entered into the input element.
+//The input element's text should be cleared when the button is pressed.//
 
-let newElement = document.createElement('p');
-newElement.id=document.getElementById('p-input').value;
-newElement.setAttribute('id', paragraphld++);
+
+let paragraphld = 0;
+document.getElementById(`p-button`).addEventListener(`click`,()=>{
+let parent= document.getElementById('p-button');//I had p-div
+
+let newElement = document.createElement(`p`);
+newElement.textContent=document.getElementById(`p-input`).value; //I had id there
+newElement.setAttribute(`id`, paragraphld++);
 
 parent.appendChild(newElement);
-document.getElementById('p-input').value=''; //changed to .value from docu.getelement
+document.getElementById(`p-input`).value=''; //changed to .value from docu.getelement
 });
